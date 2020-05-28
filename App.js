70b7d964a158257ef835731f6ad39935f9dd1f25
Guilherme Almeida <guilherme.almeida.eng@gmail.com>
2020-05-28 08:56:22 -0300

    first commit

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import SearchScreen from './src/screens/SearchScreen'
 
-export default function App() {
+const Stack = createStackNavigator();
+
+function App(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Business Search">
+        <Stack.Screen
+          name="Business Search"
+          component = {SearchScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
