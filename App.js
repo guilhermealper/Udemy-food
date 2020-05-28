8a5d5fcb0c0727480773b6f9f2975734ced33ea9
Guilherme Almeida <guilherme.almeida.eng@gmail.com>
2020-05-28 16:36:29 -0300

    90

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import SearchScreen from './src/screens/SearchScreen'
+import SearchScreen from './src/screens/SearchScreen';
 
 const Stack = createStackNavigator();
 
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import SearcBar from '../components/SearchBar';
+import SearchBar from '../components/SearchBar';
 
 const SearchScreen = ({navigation}) => {
     return (
       <View style={styles.container}>
+        <SearchBar/>
         <Text>SearchScreen</Text>
       </View>
     );
