672e1ae03caab7899f305c4c5b18e0dd40a0c777
Guilherme Almeida <guilherme.almeida.eng@gmail.com>
2020-05-29 23:27:14 -0300

    99

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -6,17 +6,22 @@ import yelp from '../api/yelp';
 const SearchScreen = ({navigation}) => {
   const [term, setTerm] = useState('');
   const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
 
   const searchApi = async() => {
-    const response = await yelp.get('/search', {
-      params:{
-        limit:50,
-        term,
-        location:'san jose'
-      }
-    })
-    setResults(response.data.businesses);
+    try{
+      const response = await yelp.get('/search', {
+        params:{
+          limit:50,
+          term,
+          location:'san jose'
+        }
+      });
+      setResults(response.data.businesses);
+    }catch (err){
+      setErrorMessage('Something went wrong')
+    }
   };
 
   return (
@@ -25,6 +30,7 @@ const SearchScreen = ({navigation}) => {
         term={term}
         onTermChange={setTerm}
         onTermSubmit={searchApi}/>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}  
       <Text>Results size: {results.length}</Text>
     </View>
   );
